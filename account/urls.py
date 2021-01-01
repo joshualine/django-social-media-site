1d00217fe23193d2a51837d5474f84818bb43d63
Joshaline <joshuachinwendu@gmail.com>
2021-01-01 17:50:25 +0100

    Added user registration

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,6 @@ app_name = 'account'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
